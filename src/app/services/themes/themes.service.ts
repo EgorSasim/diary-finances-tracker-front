af6fbe5a0f57fc7097fed23a9792fd6e8297a154
Egor Sasim <egorsasim@mail.ru>
2024-03-31 22:24:29 +0300

    fix overlay themes issue

--- src/app/services/themes/themes.service.ts
+++ src/app/services/themes/themes.service.ts
@@ -1,14 +1,36 @@
 import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
-import { Theme } from './themes.constants';
+import { DEFAULT_THEME, Theme } from './themes.constants';
 
 @Injectable({ providedIn: 'root' })
 export class ThemesService {
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.trackOverlayInsertion();
+  }
+
+  private trackOverlayInsertion(): void {
+    const listener = (event: Event) => {
+      const node = event.target as Element;
+      if (
+        node.nodeType === Node.ELEMENT_NODE &&
+        node.className === 'cdk-overlay-container'
+      ) {
+        this.document
+          .querySelector('.cdk-overlay-container')
+          ?.setAttribute('li-theme', DEFAULT_THEME);
+        this.document.body.removeEventListener('DOMNodeInserted', listener);
+      }
+    };
+
+    this.document.body.addEventListener('DOMNodeInserted', listener);
+  }
 
   public setTheme(theme: Theme): void {
-    this.document.body
+    this.document
       .querySelector('li-theme-container')
+      ?.setAttribute('li-theme', theme);
+    this.document
+      .querySelector('.cdk-overlay-container')
       ?.setAttribute('li-theme', theme);
   }
 }
